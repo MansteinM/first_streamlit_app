fcf0f5753be05790f9a11b2ab93bb0cbbb16c5fb
Mihai Moicianu <104841143+MansteinM@users.noreply.github.com>
2022-05-09 14:18:25 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,8 +42,6 @@ try:
 except ULRError as e:
         streamlit.error()
     
-#don't run anything past here while we troubleshoot
-streamlit.stop()      
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
@@ -57,6 +55,9 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = het_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
+  #don't run anything past here while we troubleshoot
+streamlit.stop()      
+  
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
